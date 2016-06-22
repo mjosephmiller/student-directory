20689bb4055c8f4b665f1dac0e3efca628cd8170
Mikey <mjosephmiller.uk@gmail.com>
2016-06-22 15:55:45 +0100

    refactored Ruby file with variable

--- directory.rb
+++ directory.rb
@@ -1,3 +1,6 @@
+student_count = 11
+
+
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +14,4 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-print "Overall, we have "
-print 11
-puts " great students"
+puts "Overall, we have #{student_count} great students"
