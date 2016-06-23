ff88c6d4d00e2e764389746a7550b3a6562541b7
Mikey <mjosephmiller.uk@gmail.com>
2016-06-23 12:33:26 +0100

    Used each_with_index to puts a number with the list of students' names

--- directory.rb
+++ directory.rb
@@ -1,16 +1,20 @@
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finsh, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+
+  name = gets.chomp
+  end
+
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -18,8 +22,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -27,7 +31,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
